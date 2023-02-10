32b4e267ca486b595d612a4878e236149f9294db
unknown <waedmansour1222006@gmail.com>
2023-02-10 13:13:40 +0200

    adding . at the end of some wisdoms & and proverbs

--- wisdom.go
+++ wisdom.go
@@ -21,8 +21,8 @@ var Wisdoms = []*Wisdom{
 	{"Knowing yourself is the beginning of all wisdom.", "Aristotle"},
 	{"The only true wisdom is in knowing you know nothing.", "Socrates"},
 	{"Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.", "Rumi"},
-	{"Show all your love on your friend, but do not show all your trust on him", "Imam Ali"},
-	{"Whoever touches his heart the exaggerated love for life, his heart will be surrounded by three things: worry that he does not lose, a desire that he does not leave, and hope that he does not realize", "Imam Ali"},
-	{"Too much laughter kills the heart", "Prophet Mohamad"},
-	{"Speak only when your words are more beneficial than your silence", "Imam Ali"},
+	{"Show all your love on your friend, but do not show all your trust on him.", "Imam Ali"},
+	{"Whoever touches his heart the exaggerated love for life, his heart will be surrounded by three things: worry that he does not lose, a desire that he does not leave, and hope that he does not realize.", "Imam Ali"},
+	{"Too much laughter kills the heart.", "Prophet Mohamad"},
+	{"Speak only when your words are more beneficial than your silence.", "Imam Ali"},
 }
